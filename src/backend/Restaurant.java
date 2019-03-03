dec9aa3b9674a0fc8258f24362d8c47d907b8491
David Thompson <thompd10@mcmaster.ca>
2019-03-02 23:50:03 -0500

    Adding .gitignore for the data folder

--- src/backend/Restaurant.java
+++ src/backend/Restaurant.java
@@ -7,8 +7,11 @@ public class Restaurant implements Comparable<Restaurant> {
 	private String name;  // The name of the Restaurant
 	private String id;  // The unique identifier for the Restaurant from the file
 	private Location loc;  // The address of the Restaurant
-	private Map<String, String> attributes;
+	private Map<String, String> attributes; // A map object that maps an attribute name
+											// to an attribute file
 	
+	
+	// TODO: accept the attribute mapping and store it, finish API
 	public Restaurant(String name, String id, Location loc) {
 		this.name = name;
 		this.id = id;
