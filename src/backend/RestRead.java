fe5adb0de46c63e26578316a102d5d15e39b79f8
thakralr <thakralr@mcmaster.ca>
2019-03-02 22:08:57 -0500

    added src

---
+++ src/backend/RestRead.java
@@ -0,0 +1,11 @@
+package backend;
+
+import org.json.simple.parser.JSONParser;
+
+public class RestRead {
+	
+	public static void ReadRestaurants() {
+		JSONParser parser = new JSONParser();
+	}
+}
+
