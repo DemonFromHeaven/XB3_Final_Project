f87259eee5ae298d05521f30cdf547ca368b65c8
David Thompson <thompd10@mcmaster.ca>
2019-03-21 09:55:04 -0400

    Added a file with filepaths in it for use

---
+++ src/backend/Filepaths.java
@@ -0,0 +1,10 @@
+package backend;
+
+public class Filepaths {
+
+	public static final String REVIEW_FILEPATH_FILTERED = "data/reviewFiltered.json";
+	public static final String REVIEW_FILEPATH = "data/review.json";
+	public static final String BUSINESS_FILEPATH = "data/business.json";
+	public static final String BUSINESS_FILEPATH_FILTERED = "data/businessFiltered.json";
+	
+}
